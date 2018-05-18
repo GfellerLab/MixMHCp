d108b435c4b6ffe50fccebedf16445ee5606ac23
DavidGfeller <davidgfeller@wl6s-157-155.unil.ch>
2018-05-18 09:02:51 +0200

    R script

--- lib/plot_length.R
+++ lib/plot_length.R
@@ -12,13 +12,31 @@ for(i in 1:ncl){
   m <- read.table(paste(cm[1],"weights/weights_", i,".txt", sep=""), header=T)
 
   for(j in 1:i){
+    sum <- sum(m[,i+j+2+trash])
+    if(sum>0){
+      tm <- m[,i+j+2+trash]/sum
+    } else {
+      tm <- rep(0,length(m[,i+j+2+trash]))   
+    }
     png(paste(cm[1],"weights/plots/lg_",i,"_",j,".png", sep=""))
-    plot(m[,1], m[,i+j+2+trash], xlab="", ylim=c(0,1), ylab="", cex.axis=1.5, yaxt="n", xaxt="n", type="b")
+    plot(m[,1], tm, xlab="", ylim=c(0,1), ylab="", cex.axis=1.5, yaxt="n", xaxt="n", type="b")
     axis(2, at=lb,labels=lb, las=1, cex.axis=1.5, cex=1.5)
     axis(1, at=min:max,labels=min:max, las=1, cex.axis=1.5, cex=1.5)
-    title(paste("Motif ", j, sep=""), xlab="Peptide length", ylab="", cex.lab=2, cex.main=3)
+    title(paste("Motif ", j, ", N=",sum, sep=""), xlab="Peptide length", ylab="", cex.lab=2, cex.main=3)
     dev.off()
   }
-  
-
+  if(trash==1){
+    sum <- sum(m[,2*i+3+trash])
+    if(sum>0){
+      tm <- m[,2*i+3+trash]/sum
+    } else {
+      tm <- rep(0,length(m[,i+j+2+trash]))   
+    }
+    png(paste(cm[1],"weights/plots/lg_",i,"_Trash.png", sep=""))
+    plot(m[,1], tm, xlab="", ylim=c(0,1), ylab="", cex.axis=1.5, yaxt="n", xaxt="n", type="b")
+    axis(2, at=lb,labels=lb, las=1, cex.axis=1.5, cex=1.5)
+    axis(1, at=min:max,labels=min:max, las=1, cex.axis=1.5, cex=1.5)
+    title(paste("Trash, N=",sum, sep=""), xlab="Peptide length", ylab="", cex.lab=2, cex.main=3)
+    dev.off()
+  }
 }
